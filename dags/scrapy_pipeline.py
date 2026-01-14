971fc359eba16a6cefe672a49002aea1b9975b42
lucas <lucasrangel2011@gmail.com>
2026-01-14 14:13:31 +0000

    airflow update

--- dags/scrapy_pipeline.py
+++ dags/scrapy_pipeline.py
@@ -1,6 +1,8 @@
 from airflow import DAG
-from airflow.operators import BashOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime
+
+project_path = '/opt/airflow/project'
 
 with DAG(
     dag_id="scrapy_pipeline",
@@ -11,6 +13,26 @@ with DAG(
     
     #1: Scrapy
     scrape_data = BashOperator(
-        task_id = 'run_scrapy',
-        bash_command = 'cd /opt/airflow/project && scrapy crawl cute_mercado_livre_spider'
+        task_id='run_scrapy',
+        bash_command=f'cd {project_path} && scrapy crawl cute_mercado_livre_spider'
     )
+
+    #2: DBT Staging
+    dbt_staging = BashOperator(
+        task_id='run_dbt_part1',
+        bash_command=f'cd {project_path}/dbt_project && dbt run --select stg_data --profiles.dir .'
+    )
+
+    #3: Great Expectations
+    gx_validate = BashOperator(
+        task_id='run_gx',
+        bash_command=f'python3 {project_path}/run_validation.py'
+    )
+
+    #4: DBT Marts
+    dbt_marts = BashOperator(
+        task_id='run_dbt_part2',
+        bash_command=f'cd {project_path}/dbt_project && dbt run --select dim_products fact_listings --profiles-dir .'
+    )
+
+    scrape_data >> dbt_staging >> gx_validate >> dbt_marts
